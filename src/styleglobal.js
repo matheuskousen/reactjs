cca8783b46f3844a0a1c2cee226dd08516c241b5
matheus kousen <matheuskousen@.com>
2020-05-12 00:47:59 -0300

    my config favorite,  i was create npx-create-react-app and , i was i added styled-component and react-router

---
+++ src/styleglobal.js
@@ -0,0 +1,39 @@
+import { createGlobalStyle } from "styled-components";
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700;900&display=swap');
+*{
+    margin:0;
+    padding:0;
+    box-sizing:border-box;
+    outline: 0;
+    &:focus {
+    outline: 0
+  }
+}
+
+html,body{
+    -webkit-font-smoothing: antialiased !important;
+}
+
+body{
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size:16px;
+}
+
+button,input{
+    font-family: 'Source Sans Pro', sans-serif;
+}
+
+a{
+    text-decoration:none;
+
+}
+ul {
+    list-style: none;
+  }
+  button {
+    cursor: pointer;
+  }
+
+`;
